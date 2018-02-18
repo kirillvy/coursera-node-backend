05243cc0dfb65668a40c19162092b6c31a6095b6
Kirill Vysokolov <kir2250@gmail.com>
2018-02-18 15:05:04 +0300

    homework 2

---
+++ models/promotions.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const promoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Currency,
+        required: true,
+        min: 0
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+
+})
+
+var Promotions = mongoose.model('Promotion', promoSchema);
+
+module.exports = Promotions;
